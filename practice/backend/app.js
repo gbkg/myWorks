eb1656fca7de3625eda22558687855c31ec4b3f6
gbkg <gkagansky@gmail.com>
2019-09-05 12:34:03 +0300

    new

--- practice/backend/app.js
+++ practice/backend/app.js
@@ -18,42 +18,48 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
-    'Access-Control-Allow-Headers', 
-    'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
-    res.setHeader('Access-Control-Allow-Methodes', 'GET, POST, DELETE, OPTIONS');
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, DELETE, OPTIONS"
+    );
     next();
-});
+  });
 
 app.post('/api/posts', (req, res, next) => {
     const post = new Post({
         title: req.body.title,
         content: req.body.content
     });
-    post.save();
-    res.status(201).json({
-        message: 'Post added successfully'
+    post.save().then(createdPost => {
+        res.status(201).json({
+            message: 'Post added successfully',
+            postId: createdPost._id
+        });
+    });
+
+});
+
+app.get('/api/posts', (req, res, next) => {
+Post.find().then(documents => {
+    res.status(200).json({
+        massage: 'posts fatched successfully',
+        posts: documents
     });
 });
 
-app.use('/api/posts', (req, res, next) => {
-    const posts = [
-        {id: 'dcsdc', 
-        title: 'First server-side post', 
-        content: 'This is coming from the server'
-    },
-    {id: 'dfhfgjd', 
-    title: 'Second server-side post', 
-    content: 'This is coming from the server!'
-}
+});
 
-    ];
-    res.status(200).json({
-        massage: 'posts fatched successfully',
-        posts: posts
-    });
+app.delete("/api/posts/:id", (req, res, next) => {
+    Post.deleteOne({_id: req.params.id}).then(result => {
+        console.log(result);
+        res.status(200).json({massage: 'Post deleted'});   
+});
+
 });
 
 module.exports = app; //export app
--- practice/src/app/app.module.ts
+++ practice/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -28,7 +29,8 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     MatButtonModule,
     MatToolbarModule,
     MatExpansionModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
